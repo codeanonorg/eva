26ef0ce6c2487b47610e9a68ccd66f72e50fbf80
Arthur Correnson <arthur.correnson@gmail.com>
2019-10-31 01:06:09 +0100

    add 2 pass post processing of labels

--- src/asm/src/main.ml
+++ src/asm/src/main.ml
@@ -1,4 +1,4 @@
-
+open Instructions
 
 let write_to_file oc i =
   output_char oc (char_of_int ((i lsr 24) land 0xFF));
@@ -7,7 +7,28 @@ let write_to_file oc i =
   output_char oc (char_of_int (i land 0xFF))
 
 
-(* test *)
+let get_adresses instr_list =
+  let rec step l i adr_list =
+    match l with
+    | [] -> adr_list
+    | LABEL (`Label s) :: tail -> step tail i ((s, i)::adr_list)
+    | _ :: tail -> step tail (i+1) adr_list
+  in
+  step instr_list 0 []
+
+
+let set_adresses instr_list =
+  let adr_list = get_adresses instr_list in
+  let replace instr =
+    match instr with
+    | LDR_R_AL (`Reg r1, `Label s) -> LDR_R_AC (`Reg r1, `Adr (List.assoc s adr_list))
+    | STR_R_AL (`Reg r1, `Label s) -> STR_R_AC (`Reg r1, `Adr (List.assoc s adr_list))
+    | _ -> instr
+  in
+  List.map replace instr_list
+  |> List.filter (function LABEL _ -> false | _ -> true)
+
+
 let _ =
   let l = ref [] in
   let oc = open_out "test.evasm" in
@@ -16,8 +37,11 @@ let _ =
     try
       l := !l @ [ Parser.instruction (Lexing.from_channel stdin) ]
     with 
-      | Parser.Eof     ->
-        List.iter (fun x -> Instructions_processing.to_bin x |> write_to_file oc) !l;
+      | Parser.Eof ->
+        let open Instructions_processing in
+        let instr = set_adresses !l in
+        let process i = to_bin i |> write_to_file oc in
+        List.iter process instr;
         exit 0
       | Parser.Error e -> print_endline e
   done
